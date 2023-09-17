745b11206b998730dfff3c502b9b85f86c11f108
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-17 09:14:10 +0100

    updated the test suite

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module for testing DB storage"""
+import unittest
+from models.base_model import BaseModel
+from models import storage
+import os
+
+
+class test_DBStorage(unittest.TestCase):
+    """ a class that to test the DB storage method """
+
+    def setUp(self):
+        """ Set up test environment """
+
+    def tearDown(self):
+        """ Remove storage file at end of tests """
+        """cleanup actions go here"""
+
+    def test_new(self):
+        """ test case for creation of newly created instances """
+
+    def test_all(self):
+        """ test case for the proper return of all instances of a class """
+
+    def test_save(self):
+        """ test case for DBStorage save method """
+
+    def test_reload(self):
+        """ test case for reloading the DB storage """
