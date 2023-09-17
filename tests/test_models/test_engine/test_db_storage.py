87fbdc9721509a3e5401d0821bbfbd321ac1dae2
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-17 18:39:40 +0100

    fixed merge conflit

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -16,13 +16,22 @@ import inspect
 import os
 import unittest
 
-DBStorage = db_storage.DBStorage
 known_classes = {"Amenity": Amenity, "City": City, "Place": Place,
                  "Review": Review, "State": State, "User": User}
 
+user = os.environ.get('HBNB_MYSQL_USER')
+pwd = os.environ.get('HBNB_MYSQL_PWD')
+host = os.environ.get('HBNB_MYSQL_HOST')
+database = os.environ.get('HBNB_MYSQL_DB')
+env = os.environ.get('HBNB_ENV')
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+DBStorage = None
+if storage_type == "db":
+    DBStorage = db_storage.DBStorage
+
+
 class test_DBStorage(unittest.TestCase):
     """ a class that tests the DB storage method """
-
     @classmethod
     def setUp(cls):
         """ Set up test environment """
@@ -30,6 +39,8 @@ class test_DBStorage(unittest.TestCase):
 
     def test_docstring(self):
         """Test for the DBStorage class docstring"""
+        if not DBStorage:
+            return
         self.assertIsNot(DBStorage.__doc__, None)
         self.assertTrue(len(DBStorage.__doc__) >= 1,
                         "DBStorage class needs a docstring")
@@ -61,6 +72,9 @@ class test_DBStorage(unittest.TestCase):
 
     def test_save(self):
         """ test case for DBStorage save method """
+
+    def test_delete(self):
+        """ test case for DBStorage delete method """
 
     def test_reload(self):
         """ test case for reloading the DB storage """
