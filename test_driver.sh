f5f60a5223250356302ce221036f93688136d627
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-17 18:25:09 +0100

    automate the execution of the test driver

---
+++ test_driver.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+HBNB_ENV=test HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db HBNB_TYPE_STORAGE=db  python3 -m unittest discover tests
