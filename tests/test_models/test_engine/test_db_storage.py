4ca03b50959d2564573076916fcdc67322c70ad9
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-18 09:12:03 +0100

    slight update to the test suite

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -24,9 +24,9 @@ pwd = os.environ.get('HBNB_MYSQL_PWD')
 host = os.environ.get('HBNB_MYSQL_HOST')
 database = os.environ.get('HBNB_MYSQL_DB')
 env = os.environ.get('HBNB_ENV')
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+
 DBStorage = None
-if storage_type == "db":
+if env_value == "db":
     DBStorage = db_storage.DBStorage
 
 if env_value == 'db':
