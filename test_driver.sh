eceb8590c445aa70de7f742ba80070924e96429b
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-20 08:11:06 +0100

    updated the test driver

--- test_driver.sh
+++ test_driver.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-HBNB_ENV=test HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db HBNB_TYPE_STORAGE=db  python3 -m unittest discover tests
+HBNB_ENV=test HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db HBNB_TYPE_STORAGE=db python3 -m unittest discover tests 2>&1 /dev/null | tail -n 1
