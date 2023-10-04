62e1a5821c3d603e57c511963724f25ae7f37776
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-04 15:03:56 -0700

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A Fabric script that generates a .tgz archive from the contents of the web_static folder 
+"""
+
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """A function that generates a .tgz archive"""
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+
+    src_folder = "web_static"
+    archive_name = f"web_static_{timestamp}.tgz"
+
+    local(f"tar -czvf {archive_name} {src_folder}")
+
+if __name__ == '__main__':
+    do_pack()
