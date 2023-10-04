dd6628fc2edace870e4239e0e6b60b7955d6d307
H. Sabah <sabahhasbi00@gmail.com>
2023-10-04 13:02:45 +0000

    Files added: task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static.
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo 'yo mama' > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+old="server_name _;"
+new="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
+sudo sed -i "s|$old|$new|" /etc/nginx/sites-enabled/default
+sudo service nginx restart
