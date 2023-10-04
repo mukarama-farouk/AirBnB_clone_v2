fbe449d75e40c17d0099771f54a79bf251f02d86
H. Sabah <sabahhasbi00@gmail.com>
2023-10-04 13:27:29 +0000

    Code fix: added nginx installation lines

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static.
+sudo apt-get update
+sudo apt-get -y install nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo 'yo mama' > /data/web_static/releases/test/index.html
