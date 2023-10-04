acb61f56a19684c50e7ae5e1e91567497b93ffe1
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-04 12:41:00 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,8 +11,8 @@ echo "<!DOCTYPE html>
   <body>
     <p>Nginx server test</p>
   </body>
-</html>" | tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data
+chown -R ubuntu:ubuntu /data/
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
