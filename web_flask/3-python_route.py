ab4391a81de919f991cf63a64921ea04bdade017
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-20 18:18:55 +0000

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module writes a script that starts a
-web app using Flask
-"""
+web app using Flask"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,28 +9,28 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-	"""Displays 'Hello HBNB'"""
-	return 'Hello HBNB!'
+    """Displays 'Hello HBNB'"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def display():
-	"""Displays 'HBNB'"""
-	return 'HBNB'
+    """Displays 'HBNB'"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>')
 def display_c_is_fun(text):
-	"""Displays 'C ' followed by the value of the text variable"""
-	return 'C {}'.format(text.replace('_', ' '))
+    """Displays 'C ' followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
 def python_is_cool(text="is cool"):
-	"""Displays 'python' followed by the value of the text variable"""
-	return 'Python {}'.format(text.replace('_', ' '))
+    """Displays 'python' followed by the value of the text variable"""
+    return 'Python {}'.format(text.replace('_', ' '))
 	
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
