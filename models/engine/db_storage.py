3f0b2ce21a6b8796b74e01729cbe8f282d7a9c9d
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-21 13:22:32 +0000

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,102 +1,84 @@
 #!/usr/bin/python3
-"""a module that defines the database storage implementation"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy import MetaData
-import os
-
-user = os.environ.get("HBNB_MYSQL_USER")
-pwd = os.environ.get("HBNB_MYSQL_PWD")
-host = os.environ.get("HBNB_MYSQL_HOST")
-database = os.environ.get("HBNB_MYSQL_DB")
-env = os.environ.get("HBNB_ENV")
+""" new class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy import (create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class DBStorage:
-    """An implementation of the Database Storage"""
-
+    """ create tables in environmental"""
     __engine = None
     __session = None
-    Session = None
 
     def __init__(self):
-        """the class constructor for the database storage implementation"""
-        self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}/{}".format(user, pwd, host, database),
-            pool_pre_ping=True,
-        )
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
         if env == "test":
-            metadata = MetaData()
-            metadata.drop_all(self.__engine, checkfirst=False)
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """a public instance method that returns a dictionary
-        consisting of all queried class from the database"""
-        from models.amenity import Amenity
-        from models.user import User
-        from models.place import Place
-        from models.state import State, Base
-        from models.city import City, Base
-        from models.review import Review
-
-        if cls is None:
-            cls = [State, City, User, Place, Review, Amenity]
-            query = []
-            for c in cls:
-                query.extend(self.__session.query(c).all())
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
         else:
-            query = self.__session.query(cls).all()
-        cls_objs = {}
-        for obj in query:
-            cls_objs[obj.to_dict()["__class__"] + "." + obj.id] = obj
-        return cls_objs
+            lista = [State, City, User, Place, Review, Amenity]
+            for clase in lista:
+                query = self.__session.query(clase)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
 
     def new(self, obj):
-        """a public instance method that adds a
-        new object to a pending state of the database transaction"""
+        """add a new element in the table
+        """
         self.__session.add(obj)
 
     def save(self):
-        """a public instance method that persists the actions
-        performed in the current transaction"""
+        """save changes
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """a public instance method that deletes a created instance
-        from the database"""
+        """delete an element in the table
+        """
         if obj:
-            self.__session.delete(obj)
-
-    def call(self, string):
-        """a public instance method used for executing
-        sql commands on the class's engine"""
-        self.__engine.execute(string)
-
-    def start_session(self):
-        """a public instance method used for starting a new session"""
-        self.__session = DBStorage.Session()
-
-    def stop_session(self):
-        """a public instance method used for ending a session"""
-        self.save()
-        self.__session.close()
+            self.session.delete(obj)
 
     def reload(self):
-        """a public instance method that initializes
-        a thread-safe version of a session"""
-        from models.user import User
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.state import State, Base
-        from models.city import City, Base
-        from models.review import Review
-
+        """configuration
+        """
         Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
 
-        DBStorage.Session = scoped_session(
-            sessionmaker(bind=self.__engine, expire_on_commit=False)
-        )
-        self.__session = DBStorage.Session()
     def close(self):
-        """Calls remove()"""
+        """ calls remove()
+        """
         self.__session.close()
